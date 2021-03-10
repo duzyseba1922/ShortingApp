c97455ea9b9d7bf2251460cf0380a48f65e21b30
Sebastian Niestój <seba-19971225@o2.pl>
2021-03-10 09:55:59 +0100

    Initial Commit

---
+++ ShortingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShortingApp
+//
+//  Created by Sebastian Niestój on 10/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
